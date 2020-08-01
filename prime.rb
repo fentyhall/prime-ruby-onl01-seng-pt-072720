e4851f93c18f2d94ab3f29c486daa9335b5012c5
fentyhall <fentymhall@gmail.com>
2020-07-31 20:19:23 -0700

    Done.

--- prime.rb
+++ prime.rb
@@ -1,21 +1,9 @@
 def prime?(n)
-    if n <= 1
-      false
-    else 
-        if (2...n).any? { |i| n % i  == 0}
-            false
-        else
-            true
-        end 
-    end
-end
-
-# def prime?(n)
-#     if n <= 1 
-#         false
-#     elsif (2..n).any? { |i| n % i == 0 }
-#         false 
-#     else
-#         true
-#     end 
-# end 
+    if n <= 1 
+        false
+    elsif (2..n-1).any? { |i| n % i == 0 }
+        false 
+    else
+        true
+    end 
+end 
